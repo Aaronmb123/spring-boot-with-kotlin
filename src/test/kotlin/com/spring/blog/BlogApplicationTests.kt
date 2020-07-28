df58b19b6870bce0022e3bf3ec9aece912f0190c
Aaronmb123 <bakeaaro@oregonstate.edu>
2020-07-28 06:43:22 -0700

    added setup/teardown methods to blog test

--- src/test/kotlin/com/spring/blog/BlogApplicationTests.kt
+++ src/test/kotlin/com/spring/blog/BlogApplicationTests.kt
@@ -1,6 +1,8 @@
 package com.spring.blog
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -8,14 +10,25 @@ import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.HttpStatus
 
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BlogApplicationTests(@Autowired val restTemplate: TestRestTemplate) {
+
+	@BeforeAll
+	fun setup() {
+		println(">> Setup")
+	}
 
 	@Test
 	fun `Assert blog page title, content and status code`() {
 		val entity = restTemplate.getForEntity<String>("/")
 		assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
 		assertThat(entity.body).contains("<h1>Blog</h1>")
+	}
+
+	@AfterAll
+	fun tearDown() {
+		println(">> Tear Down")
 	}
 
 }
